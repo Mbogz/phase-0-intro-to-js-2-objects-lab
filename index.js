7f292925e2939c824b6a6534d757da8ea3ec12b1
Mbogz <jeff.mwaura@student.moringaschool.com>
2023-04-12 11:00:52 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution in this file!
+var employees = {};
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    for (const key in employee) {
+        delete employee[key];
+      }
+
+      const name = Sam;
+      const streetAddress = "11 Broadway";
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee(); {
+        const name = Sam;
+      const streetAddress = "11 Broadway";
+      return employee;
+    }
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    let newEmployee = deleteFromEmployeeByKey(employee, 'name');
+    typeofnewEmployee = Sam;
+    return newEmployee;
+}
+
+function deleteFromEmployeeByKey(employee, Sam);{
+    employee = Sam;
+    return employee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    let newEmployee = destructivelyDeleteFromEmployeeByKey(employee, Sam);
+    employee == Sam;
+    employee == newEmployee;
+    return newEmployee;
+}
